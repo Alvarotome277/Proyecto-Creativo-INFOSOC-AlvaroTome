63471de9eef5e22998803eaa1c27491578feaced
Alvarotome277 <alvarotomemolina2@gmail.com>
2026-04-23 14:19:26 +0200

    Final

--- Votacion_Digital.c
+++ Votacion_Digital.c
@@ -1,87 +1,142 @@
 #include <stdio.h>
 #include <string.h>
+
 #define MAX_WORD 100
 #define MAX_VOTANTES 100000
 #define MAX_CANDIDATOS 100
-typedef struct {
-long votos;
-char nombre[MAX_WORD]
-}Candidato;
 
-
-
-
-int main(){
-Candidato candidatos[MAX_CANDIDATOS];
-int i, encontrado = 0, j;
-char candidato_temp[MAX_WORD];
-long n_candidatos = MAX_CANDIDATOS + 1, n_votantes = MAX_VOTANTES + 1;
-while (n_candidatos > MAX_CANDIDATOS)
+typedef struct
 {
-    printf ("Introduce el numero (menor que %ld) de candidatos : ", MAX_CANDIDATOS);
-    scanf (" %ld", &n_candidatos);
-    if (n_candidatos > MAX_CANDIDATOS)
-    {
-        printf("El numero de candidatos tiene que ser menor que %ld", MAX_CANDIDATOS);
-    }
-    
-}
+    long votos;
+    char nombre[MAX_WORD];
+} Candidato;
 
-
-for ( i = 0; i < n_candidatos; i++)
+int main()
 {
-    printf ("Introduce el nombre del candidato %ld: ",i + 1);
-    scanf (" %s", candidatos[i].nombre);
-    candidatos[i].votos = 0;
-    for ( j = 0; j < i; j++)
+    Candidato candidatos[MAX_CANDIDATOS];
+    int i, j, encontrado = 0;
+    char candidato_temp[MAX_WORD];
+    long n_candidatos = MAX_CANDIDATOS + 1, n_votantes = MAX_VOTANTES + 1;
+
+    while (n_candidatos > MAX_CANDIDATOS || n_candidatos <= 0)
     {
-        if (strcmp(candidatos[j].nombre, candidatos[i].nombre) == 0)
-        {
-            printf (" Candidato ya registrado\n");
-            i--;
-            encontrado = 1;
-        }
-        else if ((j == i - 1 && encontrado == 0 )||( j == 0 && encontrado == 0)){
-        printf ("%s Introducido a la votacion\n", candidatos[i].nombre);
+        printf("Introduce el numero de candidatos (1 - %d): ", MAX_CANDIDATOS);
+        scanf(" %ld", &n_candidatos);
+        if (n_candidatos > MAX_CANDIDATOS || n_candidatos <= 0){
+            printf("El numero debe estar entre 1 y %d.\n", MAX_CANDIDATOS);
         }
     }
-    
-    
-    
-}
-while (n_votantes > MAX_VOTANTES)
-{
-    printf ("Introduce el numero (menor que %ld) de votantes: ", MAX_VOTANTES );
-    scanf (" %ld" , &n_votantes);
-    if (n_votantes > MAX_VOTANTES)
+
+    for (i = 0; i < n_candidatos; i++)
     {
-        printf("El numero de votantes tiene que ser menor que %ld", MAX_VOTANTES);
-    }
-    
-}
-for ( i = 0; i < n_votantes; i++)
-{
-    encontrado = 0;
-    while (encontrado == 0)
-    {
-    printf ("Introduce el nombre del candidato, eres el votante numero %ld: ",i + 1);
-    scanf (" %s", candidato_temp);
-    for ( i = 0; i < n_candidatos; i++)
+        printf("Introduce el nombre del candidato %d: ", i + 1);
+        scanf(" %s", candidatos[i].nombre);
+        candidatos[i].votos = 0;
+
+        encontrado = 0;
+        for (j = 0; j < i; j++)
         {
-            if (strcmp (candidato_temp, candidatos[i].nombre) == 0)
+            if (strcmp(candidatos[j].nombre, candidatos[i].nombre) == 0)
             {
-                candidatos[i].votos ++;
+                printf("  ¡Candidato ya registrado! Intentalo de nuevo.\n");
                 encontrado = 1;
-                printf("Voto añadido a %s", candidatos[i].nombre);
+                break;
             }
-            else if ( i == n_candidatos - 1 && encontrado == 0){
-                printf (" Candidato no encontrado");
-            }
-        }    
+        }
+
+        if (encontrado)
+        {
+            i--;
+        }
+
+        else
+        {
+            printf("  '%s' registrado correctamente.\n", candidatos[i].nombre);
+        }
     }
-}
 
+    while (n_votantes > MAX_VOTANTES || n_votantes <= 0)
+    {
+        printf("Introduce el numero de votantes (1 - %d): ", MAX_VOTANTES);
+        scanf(" %ld", &n_votantes);
+        if (n_votantes > MAX_VOTANTES || n_votantes <= 0)
+        {
+            printf("El numero debe estar entre 1 y %d.\n", MAX_VOTANTES);
+        }
+    }
 
+    for (i = 0; i < n_votantes; i++)
+    {
+        encontrado = 0;
+        while (!encontrado)
+        {
+            printf("Votante %d, introduce el nombre de tu candidato: ", i + 1);
+            scanf(" %s", candidato_temp);
+            for (j = 0; j < n_candidatos; j++)
+            {
+                if (strcmp(candidato_temp, candidatos[j].nombre) == 0)
+                {
+                    candidatos[j].votos++;
+                    encontrado = 1;
+                    printf("  Voto agregado a '%s'.\n", candidatos[j].nombre);
+                    break;
+                }
+            }
+            if (!encontrado)
+            {
+                printf("  Candidato no encontrado. Prueba de nuevo.\n");
+            }
+        }
+    }
 
-    
+    printf("\n========== RESULTADOS ==========\n");
+
+    for (i = 0; i < n_candidatos - 1; i++)
+    {
+        for (j = 0; j < n_candidatos - 1 - i; j++)
+        {
+            if (candidatos[j].votos < candidatos[j + 1].votos)
+            {
+                Candidato temp = candidatos[j];
+                candidatos[j] = candidatos[j + 1];
+                candidatos[j + 1] = temp;
+            }
+        }
+    }
+
+    for (i = 0; i < n_candidatos; i++)
+    {
+        printf("  %d. %-20s %ld votos\n", i + 1, candidatos[i].nombre, candidatos[i].votos);
+    }
+
+    printf("\n=================================\n");
+    long max_votos = candidatos[0].votos;
+    int n_ganadores = 0;
+    for (i = 0; i < n_candidatos; i++)
+    {
+        if (candidatos[i].votos == max_votos)
+        {
+            n_ganadores++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (n_ganadores == 1)
+    {
+        printf("  GANADOR: %s con %ld votos.\n", candidatos[0].nombre, max_votos);
+    }
+    else
+    {
+        printf("  EMPATE entre %d candidatos con %ld votos:\n", n_ganadores, max_votos);
+        for (i = 0; i < n_ganadores; i++)
+        {
+            printf("    - %s\n", candidatos[i].nombre);
+        }
+    }
+    printf("=================================\n");
+
+    return 0;
 }
